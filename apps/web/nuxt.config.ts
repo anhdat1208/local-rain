76cb72bb69961bfa36cd38bcbb5f9b2e3e84575b
Dat <datvoanh95@gmail.com>
2026-07-29 16:42:06 +0700

    update PWA

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -88,6 +88,18 @@ export default defineNuxtConfig({
           sizes: "512x512",
           type: "image/png",
         },
+        {
+          src: "/pwa-maskable-192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "/pwa-maskable-512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
       ],
     },
     workbox: {
